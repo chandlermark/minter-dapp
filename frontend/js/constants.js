0585a303893f5b81bb01ffba33cf78eb63bb5063
Mark Chandler <mark.chandler.ccv@gmail.com>
2022-07-07 15:36:24 +0100

    updated text

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Pondering Eagles NFT Collection!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Here you can mint your very own Pondering Eagle NFT!';
+const welcome_p = 'Get Ready to Mint your very own Pondering Eagle NFT!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Mint Your Pondering Eagle Now!";
