7e28cd4f862e26813029cf644189caa601fd1b47
Mark Chandler <mark.chandler.ccv@gmail.com>
2022-07-07 11:50:20 +0100

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x02451DCa3F93662Dbb290cFD6BDF0Ec9Afa0a8e3"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
